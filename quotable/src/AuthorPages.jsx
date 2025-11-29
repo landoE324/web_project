2c2824463328921433c56c276b632ba972d084e0
Drew <deandrew503@gmail.com>
2025-11-29 21:05:01 +0000

    still implementing api into quotes page

--- quotable/src/AuthorPages.jsx
+++ quotable/src/AuthorPages.jsx
@@ -8,27 +8,47 @@ import arrow from "./assets/arrow.png"
 export default function QuotesPage() {
   
   const [animating, setAnimating] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [quotes, setQuotes] = useState([]);
+
+  const contentArray = quotes.length > 0 ? [quotes[0], quotes[1], quotes[3]] : [];
+
+  const currentQuote = contentArray.length > 0 ? contentArray[currentIndex] : null;
 
   const handleNext = () => {
     setAnimating(true);
-    setTimeout(() => setAnimating(false), 300); // match CSS animation time
+    setTimeout(() => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + contentArray.length) % contentArray.length);
+    setAnimating(false);
+  }, 300);
   };
 
   const handlePrev = () => {
     setAnimating(true);
-    setTimeout(() => setAnimating(false), 300);
+    setTimeout(() => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + contentArray.length) % contentArray.length);
+    setAnimating(false);
+  }, 300);
   };
   
-  const [quote, setQuote] = useState("");
+  useEffect(() => {
+    async function loadQuotes() {
+      try {
+        const res = await fetch("https://echoes.soferity.com/api/quotes");
+        const json = await res.json();
+        setQuotes(json.data); // json.data is the array of quotes
+      } catch (err) {
+        console.error("Error fetching quotes:", err);
+      }
+    }
+
+    loadQuotes(); // call the async function
+  }, []); // empty dependency array = run ONLY once when component mounts
 
   useEffect(() => {
-    fetch("https://api.quotable.io/random")
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.author);
-      });
+  console.log("Quotes updated:", quotes[1]);
+}, [quotes]);
 
-  }, []);
 
   return (
     <div className= {styles.appContainer}>
@@ -68,14 +88,19 @@ export default function QuotesPage() {
 
       <div className={`${styles.cardContainer} ${animating ? styles.shuffle : ""}`}>
           
-          <Card 
-          image={picture} 
-          type = "Quote"
-          text = '"A journey of a thousand miles begins with a single step."'
-          title = "Lao Tzu"
-          size = "large"
-          />
-
+          {currentQuote ? (
+              <Card
+                image={picture}
+                type="Quote"
+                text={`"${currentQuote.quote}"`}
+                title={currentQuote.author}
+                size="large"
+              />
+            ) 
+            : 
+            (
+              <p>Loading...</p>
+            )}
       </div>
 
       <button className = {styles.arrow} onClick={handleNext}>
